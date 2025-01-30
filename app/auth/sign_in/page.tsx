079bb8cdf587d0b2d741dd0783c94b4f0649c54d
Alex Kagai <Alex Kagai>
2025-01-30 11:48:05 +0300

    Add the auth routes

---
+++ app/auth/sign_in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <div>sign_in</div>
+  )
+}
+
+export default SignIn;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
-import Image from "next/image";
+import Link from "next/link";
+import { ArrowRightIcon, LockOpenIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col my-auto items-center sm:items-start">
 
-        <div className="w-full md:w-2/3 flex flex-col gap-4 mb-16 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 w-full">Welcome to RMW</h1>
-          <p className="mb-8 leading-relaxed">Share a ride if you are traveling the same direction.</p>
+        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          
+          <div className="text-center lg:w-2/3 w-full">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Ride My Way</h1>
+            <p className="mb-8 leading-relaxed">Share a ride if you are going the same direction.</p>
+
+            <div className="flex justify-center gap-10 w-full">
+              <Link href="/auth/sign_in" className="flex justify-start gap-2 bg-indigo-500 hover:bg-indigo-600 
+                py-2 px-6 text-white text-lg font-semibold">
+                <span>Login</span> 
+                <span className="order-first font-bold"><LockOpenIcon className="w-5 md:w-6"/></span>
+              </Link>
+
+              <Link href="/auth/sign_up" className="flex justify-start gap-2 bg-gray-100 hover:bg-gray-200 
+                py-2 px-6 text-gray-700 text-lg font-semibold">
+                <span>SignUp</span>  
+                <span className="font-bold"><ArrowRightIcon className="w-5 md:w-6"/></span>
+              </Link>
+
+            </div>
+          </div>
+
         </div>
-        <div> SignInButton, Sign up button</div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="#" target="_blank" rel="noopener noreferrer">Developer</a>
-        <a className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="#" target="_blank" rel="noopener noreferrer">Backend docs</a>
+        <a className="flex" href="#" target="_blank" rel="noopener noreferrer">
+          Developer</a>
+        <a className="flex" href="#" target="_blank" rel="noopener noreferrer">Backend docs</a>
       </footer>
     </div>
   );
