2b7088213c939589e82db2590cd4fde0cc2512ef
Alex Kagai <Alex Kagai>
2025-01-31 17:31:34 +0300

    Update packages & dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,23 @@
+'use client';
 import React from 'react';
+import Sidebar from '@/app/dashboard/ui/sidebar';
+import Navbar from '@/app/dashboard/ui/navbar';
 
+export default function DashBoard() {
+  return (
+    <div className="flex h-screen bg-slate-100">
+      <div className="w-1/5 mr-2">
+        <Sidebar />
+      </div>
+      <div className="w-4/5">
+        <div className="bg-white mt-2 mr-2 w-9/12">
+          <Navbar/>
+        </div>
 
-export default function Page() {
-  
-    return (
-    <div className="py-2 px-2">
-      <div> Sidebar</div>
-      <div> Content</div>
+        <div className="bg-black mt-40 mr-2 text-white w-full">
+          Content
+        </div>
+      </div>
     </div>
-  )
+  );
 }
