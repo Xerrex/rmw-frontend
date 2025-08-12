32621131ff542014774ef2d1166cd39b5421d81f
Alex Kagai <alexkagai@outlook.com>
2025-08-12 17:18:14 +0300

    Add rides overview cards

---
+++ src/utils/placeholder_data.js
@@ -0,0 +1,7 @@
+export const ridesDataTotal = [
+  {title: "All Rides", value: 40, type:"allRides"},
+  {title: "Rides Offered", value: 10, type:"ridesOffered"},
+  {title: "Ride Taken", value: 10, type:"ridesTaken"},
+  {title: "Requests Pending", value: 10, type:"requestsPending"},
+  {title: "Requests Rejected", value: 10, type:"requestsRejected"}
+]
