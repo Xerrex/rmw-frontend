0a9a52de79f7c8becd93225faf3bbd656bd0c2d2
Alex Kagai <alexkagai@outlook.com>
2025-08-13 12:23:50 +0300

    Add rides search bar

--- src/Dashboard/Rides/Rides.jsx
+++ src/Dashboard/Rides/Rides.jsx
@@ -1,15 +1,40 @@
 import { useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import { useSearchParams } from "react-router";
+import { SearchOutlined} from "@ant-design/icons";
 import useHeaderContext from "../UI/Header/Context/useHeaderContext";
 
 function Rides() {
   const {setTitle} = useHeaderContext();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(()=>{
     setTitle("Rides");
   })
 
+  const handleSearch = useDebouncedCallback((searchText)=>{
+    console.log("handleSearch", searchText); // TODO: remove
+    if(searchText){
+      setSearchParams({"sort": searchText});
+    }else{
+       setSearchParams({});
+    }
+  }, 300);
+
   return (
-    <div>Rides</div>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-center">
+        <label htmlFor="search" className="sr-only"> Search</label>
+        <input className="block w-1/2 rounded-md border-gray-200 dark:border-white py-[9px] pl-10
+          mt-2 text-sm outline-2 placeholder:text-gray-500 dark:placeholder:text-white" 
+          placeholder="Search ride" onChange={(e)=>handleSearch(e.target.value)}
+           defaultValue={searchParams.get('sort')?.toString()}/>
+
+        <SearchOutlined style={{fontSize:"20px", marginLeft:"10px"}}/>
+      </div>
+
+      Rides
+    </div>
   )
 }
 
