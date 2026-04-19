f55b7f9270ef1fec4d1921a14f4c7c993dedcb3f
Alex Kagai <alexkagai@outlook.com>
2026-04-19 10:16:15 +0300

    Initial revamp

--- src/Auth/AuthContext/useAuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "./AuthProvider";
-
-
-const useAuthContext = ()=>{
-    return useContext(AuthContext)
-}
-
-export default useAuthContext;
